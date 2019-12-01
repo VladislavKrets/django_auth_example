0b57558d8917a27be7ea83f5c0faf978bd879e56
lollipop <vladislav.krets@gmail.com>
2019-12-01 22:07:17 +0300

    change group registering

--- django_test/api/views.py
+++ django_test/api/views.py
@@ -33,7 +33,8 @@ class registerHome(APIView):
         else:
             return Response(status.HTTP_403_FORBIDDEN)
 
-#register group
+
+# register group
 class registerGroup(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -43,24 +44,29 @@ class registerGroup(APIView):
         # token get or none
         token = Token.objects.get(key=token)
         user = token.user
-        if user.expandeduser.is_admin:
-            serialized = serializers.AddVKGroupSerializer(data=request.data)
-            if serialized.is_valid():
+        serialized = serializers.AddVKGroupSerializer(data=request.data)
+
+        if serialized.is_valid():
+            group = models.VKGroup.objects.filter(vk_id=serialized.validated_data['vk_id'])
+            if not group:
                 group = models.VKGroup.objects.get_or_create(user_id=user, vk_id=serialized.validated_data['vk_id'])
+                user.expandedUser.is_admin = True
+                user.save()
                 return Response(serialized.validated_data, status=status.HTTP_201_CREATED)
             else:
-                return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
+                return Response(status = status.HTTP_400_FORBIDDEN)
         else:
-            return Response(status.HTTP_403_FORBIDDEN)
+            return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-#get user
+# get user
 class getUser(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
+
     def get(self, request):
         token = request.META.get('HTTP_AUTHORIZATION').split(" ")[1]
-        #token get or none
+        # token get or none
         token = Token.objects.get(key=token)
         user = token.user
         serialized = serializers.GetUserSerializer(user)
@@ -76,11 +82,12 @@ def register(request):
         return Response({'token': token.key}, status=status.HTTP_201_CREATED)
     return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 @api_view(['POST'])
 def auth(request):
     serialized = serializers.RegisterUserSerializer(data=request.data)
     if serialized.is_valid():
-        #user get or none
+        # user get or none
         user = User.objects.get(username=serialized.validated_data['username'])
         token, created = Token.objects.get_or_create(user=user)
         return Response({'token': token.key}, status=status.HTTP_200_OK)
